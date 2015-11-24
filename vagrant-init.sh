23c0762566217a757463bb94335620779046f778
Mark Manders <mark.manders@ihomer.nl>
2015-11-24 12:06:00 +0100

    upstart script vertx bare

--- vagrant-init.sh
+++ vagrant-init.sh
@@ -3,15 +3,15 @@
 export DEBIAN_FRONTEND=noninteractive
 
 cat > /etc/default/locale <<EOF
-  LANG="en_US.UTF-8"
-  LC_ALL="en_US.UTF-8"
+LANG="en_US.UTF-8"
+LC_ALL="en_US.UTF-8"
 EOF
 
 cat > /etc/apt/sources.list <<EOF
-  deb mirror://mirrors.ubuntu.com/mirrors.txt trusty main restricted universe multiverse
-  deb mirror://mirrors.ubuntu.com/mirrors.txt trusty-updates main restricted universe multiverse
-  deb mirror://mirrors.ubuntu.com/mirrors.txt trusty-backports main restricted universe multiverse
-  deb mirror://mirrors.ubuntu.com/mirrors.txt trusty-security main restricted universe multiverse
+deb mirror://mirrors.ubuntu.com/mirrors.txt trusty main restricted universe multiverse
+deb mirror://mirrors.ubuntu.com/mirrors.txt trusty-updates main restricted universe multiverse
+deb mirror://mirrors.ubuntu.com/mirrors.txt trusty-backports main restricted universe multiverse
+deb mirror://mirrors.ubuntu.com/mirrors.txt trusty-security main restricted universe multiverse
 EOF
 
 apt-get update
@@ -22,4 +22,15 @@ apt-get -y install oracle-java8-installer oracle-java8-set-default authbind
 
 wget https://bintray.com/artifact/download/vertx/downloads/vert.x-3.1.0-full.tar.gz -O /tmp/vertx.tar.gz
 tar -vxzf /tmp/vertx.tar.gz -C /opt/
+rm -v /tmp/vertx.tar.gz
 ln -s /opt/vert.x-3.1.0/bin/vertx /usr/local/bin/vertx
+
+cat > /etc/init/vertx.conf <<EOF
+start on runlevel [2345]
+stop on runlevel [016]
+
+env JAVA_OPTS="-Djava.net.preferIPv4Stack=true"
+exec /usr/local/bin/vertx bare
+EOF
+
+start vertx
